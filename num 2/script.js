2b13e72e2317e3bfc3eb58d7da31f6a9bc6fe5ad
Melk <141427498+Melquizedek18@users.noreply.github.com>
2023-08-21 19:58:06 -0300

    Add files via upload

---
+++ num 2/script.js
@@ -0,0 +1,7 @@
+function imgSlider(someThing){
+        document.querySelector('.Cars').src = someThing;
+    }
+    function changeColor(color){
+        const sec = document.querySelector('.Home');
+        sec.style.background = color;
+    }
